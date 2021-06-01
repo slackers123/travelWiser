d335375fb529d0e115b47ae3b67c420206cdfbdf
slackers <severin.gebesmair@gmx.at>
2021-06-01 13:43:37 +0200

    Rating

--- src/Rating.java
+++ src/Rating.java
@@ -1,2 +1,11 @@
 public class Rating {
+    private float rating;
+
+    public Rating(float rating){
+        this.rating = rating;
+    }
+
+    public float getRating(){
+        return rating;
+    }
 }

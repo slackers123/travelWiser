d59506de8e18620164f1ba111e3d34465a527804
sef22392 <fabiansefranek@gmail.com>
2021-06-01 13:33:25 +0200

    setters & getters + constructor

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,4 +3,34 @@ public class Restaurant {
     private String address;
     private String city;
     private String category;
+    public Restaurant(String name, String address, String city, String category) {
+        setName(name);
+        setAddress(address);
+        setCity(city);
+        setCategory(category);
+    }
+    private void setName(String name) {
+        this.name=name;
+    }
+    private void setAddress(String address) {
+        this.address=address;
+    }
+    private void setCity(String city) {
+        this.city = city;
+    }
+    private void setCategory(String category) {
+        this.category = category;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getName() {
+        return name;
+    }
 }

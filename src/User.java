e9260d6f68ee97299579ef3962391994185281ea
sef22392 <fabiansefranek@gmail.com>
2021-06-01 13:46:36 +0200

    setters & getters + constructor

--- src/User.java
+++ src/User.java
@@ -1,2 +1,14 @@
 public class User {
+    private String username;
+    public User(String username) {
+        setUsername(username);
+    }
+
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
